064c88935653efb171d3201c2b9b188b9f852db8
Graham McAllister <gramcal@amazon.com>
2021-02-07 15:25:48 +0900

    parse test passing

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,14 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ = input;
 
-    unimplemented!()
+    let a = quote! {
+        let a = 5;
+    };
+    a.into()
 }
